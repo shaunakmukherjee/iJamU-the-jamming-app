8ab4ca0d84b16233f270f77d56263485d874b76f
Chi Shin <cyshin93@gmail.com>
2016-12-04 18:34:54 -0500

    Re-uploaded OOSE_Jamming

--- OOSE_Jamming/Jamming/Login/urls.py
+++ OOSE_Jamming/Jamming/Login/urls.py
@@ -6,14 +6,10 @@ from . import views
 # Addin an extra URL = home!
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-# URl to access data of all the user
     url(r'^post/new/$', views.usr_list, name='usr_list'),
-# Search redirects to this URL 
-    url(r'^post/$', views.profilesearch, name='profilesearch'),
-# Returns details of the selected user 
+    url(r'^post/$', views.gsearch, name='search'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-# Returns search results
-    url(r'^post/(?P<key>.*)/$', views.profile, name='profile'),
+    url(r'^post/(?P<key>.*)/$', views.ksearch, name='ksearch'),
 # URLs for Registration
     url(r'^accounts/register/$', views.register, name='register'),
     url(r'^accounts/register/complete/$', views.registration_complete, name='registration_complete'),
