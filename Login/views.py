befb05f5dd1e2354d0b0dec8534c91f0dcbaa9ab
Shaunak Mukherjee <shaunak.mukherjee@jhu.edu>
2016-12-10 19:29:15 -0500

    Reverted views.py

--- Login/views.py
+++ Login/views.py
@@ -116,7 +116,6 @@ def makereq(request, **kwargs):
     k=Crequest(User1=s,User2=pseudov)
     # Store in the database.
     k.save()
-    return render(request, 'connectionrequests/sent.html')
 
 # Delete the request from the database after the request has been either accepted or declined.
 def delreq(request, **kwargs):
@@ -127,7 +126,6 @@ def delreq(request, **kwargs):
     k=get_object_or_404(Crequest,User1=pseudov,User2=s)
     # Delete the request
     k.delete()
-    return render(request, 'connectionrequests/reject.html')
 
 # Create a connection, Adding redundancy to make querying easier.
 def accept(request, **kwargs):
@@ -142,10 +140,9 @@ def accept(request, **kwargs):
     k=Connection(User1=pseudov,User2=s)
     # Store in the database.
     k.save()
-    return render(request, 'connectionrequests/accept.html')
 
 # Delete the connection from the database.
-def deletecon(request, **kwargs):
+def delreq(request, **kwargs):
      # Get username of the user.
     for key, value in kwargs.iteritems():
         s=value;  
@@ -157,7 +154,6 @@ def deletecon(request, **kwargs):
     k=get_object_or_404(Connection,User1=s,User2=pseudov)
     # Delete the connection from the other end.
     k.delete()
-    return render(request, 'connectionrequests/deleteconnection.html')
 
 
 # Update user details.
