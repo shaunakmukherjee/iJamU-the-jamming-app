14abf3b48e41cd43b7fe3c89818649ae5dc42a8a
Chi Shin <cyshin93@gmail.com>
2016-12-12 13:41:05 -0500

    Final Login test

--- Login/test_views.py
+++ Login/test_views.py
@@ -6,30 +6,38 @@ from .models import Userdetail
 # imports of views
 from . import views
 
-# test views validations with HTTP
+# test Login and Signup
 class Test_Validations(TestCase):
+    # Install the test fixtures for user and userdetail
     fixtures = ['user_data','userdetail_data']
+    usrs = User.objects.all()
 
     def setUp(self):
         self.c = Client()
-        self.user = User.objects.create_user(username='test', password='12345')
+        
+        # set/hash the passwords of the users
+        for u in self.usrs:
+            u.set_password(u.password)
+            u.save()
     
     # LOGIN home()
     def test_login(self):
-        # login using test user
-        response = self.c.post('/login/',{'username': 'test', 'password': '12345'},follow=True)
-        
         # test for login failures
-        
-        #self.assertEqual(response.redirectchain,[('http://testserver/login/home.html',302)])
-        
+        logged = self.c.login(username='falsetest',password='00000');
+        self.assertEqual(logged,False)
+        # test for incorrect password
+        logged = self.c.login(username='test1',password='00000');
+        self.assertEqual(logged,False)
+        # login using test user
+        logged = self.c.login(username='test1',password='11111');
+        self.assertEqual(logged,True)
+
     #REGISTER register()
     def test_register(self):
         # open the register page
         response = self.c.head('/accounts/register/')
-        
         self.assertEqual(response.status_code, 200)
-        #self.assertEqual(response.templates, 'registration/registration_form.html')
+        self.assertEqual(response.templates, 'registration/registration_form.html')
         
         # Complete registration
         response = self.c.post('/accounts/register/',{'username': 'newtest',
@@ -37,7 +45,6 @@ class Test_Validations(TestCase):
         # test for invalid registration
                 
         self.assertEqual(response.status_code, 200)
-        
         #self.assertEqual(response.redirectchain,[('http://testserver/accounts/register/complete',302),
         #                                         ('post_detail',304)])
         #self.assertEqual(response.templates, 'profilesearch/post_edit.html')
