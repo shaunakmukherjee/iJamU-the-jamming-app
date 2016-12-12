7de3d721ffefbe4e76bea023365f90329adae508
Chi Shin <cyshin93@gmail.com>
2016-12-12 07:18:56 -0500

    test for login and registration

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,17 +1,48 @@
-from django.test import TestCase, RequestFactory
+# Enables use of dummy web browser
+from django.test import Testcase, Client
 from django.contrib.auth.models import User
-# imports for testing the view
-from . import views
 
-# test user profile
-class TestUserProfile(TestCase):
+# imports of views
+from /Login import views
+
+# test views validations with HTTP
+class Test_Validations(TestCase):
 
     def setUp(self):
-        self.factory = RequestFacotry()
-        self.user = User.objects.create_user(username='usertest', password='12345')
+        self.c = Client()
+        self.user = User.objects.create_user(username='test', password='12345')
     
-    # test profile creation
-    def test_create_profile(self):
+    # LOGIN home()
+    def test_login(self):
+        # login using test user
+        response = self.c.post('login/',{'username': 'test', 'password': '12345'})
+        
+        # test for login failures
+        
+        # find actual response code
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates, 'home.html')
+        
+    #REGISTER register()
+    def test_register():
+        # open the register page
+        response = self.c.post('/accounts/register/', follow=true)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates, 'registration/registration_form.html')
+        
+        # Complete registration
+        # Consider making a test fixture
+        response = self.c.post('/accounts/register/',{'Fname':'John',
+                               'Lname':'Doe','Techlevel':'1','Year':'1','Rating':'1',
+                               'Bio':'test is life','Instruments':'Guitar',
+                               'Genre':'Rock'},follow=true)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.redirectchain,[('http://testserver/accounts/register/complete',302),
+                                                 ('post_detail'),302])
         
         
-    def test_update():
+        
+        
+    
