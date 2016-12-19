e2f4ad73e8e6babe084e6be41ce6f19daeab7cea
Shaunak Mukherjee <shaunak.mukherjee@jhu.edu>
2016-12-18 19:16:45 -0500

    Update admin.py

--- Connection/admin.py
+++ Connection/admin.py
@@ -1,8 +1,10 @@
 #Connection/admin.py
 
 from django.contrib import admin
-from .models import Connection,Crequest
+from .models import Connection,Crequest, Endorsement, Endorsedetails
 
 # Register your models here.
 admin.site.register(Crequest)
 admin.site.register(Connection)
+admin.site.register(Endorsement)
+admin.site.register(Endorsedetails)
