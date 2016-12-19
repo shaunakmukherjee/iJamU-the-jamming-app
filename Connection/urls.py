d4eae5f924b723198d7f2bfce03b94f9c9e36c55
Shaunak Mukherjee <shaunak.mukherjee@jhu.edu>
2016-12-18 19:09:13 -0500

    Update urls.py

--- Connection/urls.py
+++ Connection/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     url(r'^deleteconnection/(?P<key>.*)/$',views.deleteconnection, name='deleteconnection'),
     url(r'^consearch/(?P<key>.*)/$', views.consearch, name='consearch'),
     url(r'^connections/$', views.Connections, name='connections'),
+#URLs for endorsements.
+    url(r'^endorsements/$', views.Endorsements, name='endorsements'),
+    url(r'^endsearch/(?P<key>.*)/$', views.endsearch, name='endsearch'),
 #URLs for messaging.
 	url(r'^messaging/$',views.Messaging, name='messaging'),
 	url(r'^email/$',views.Email,name='email'),
